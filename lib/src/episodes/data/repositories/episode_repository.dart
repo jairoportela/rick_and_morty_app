8714f07bfd2f6e414069ceec7ab906887aa4f8bb
Jairo Andrés Portela Cortés <jairo.portela@imagineapps.co>
2023-09-01 09:58:45 -0500

    feat: add episode repo handle

--- lib/src/episodes/data/repositories/episode_repository.dart
+++ lib/src/episodes/data/repositories/episode_repository.dart
@@ -1,15 +1,20 @@
 import 'package:rick_and_morty_app/src/core/core.dart';
 import 'package:rick_and_morty_app/src/core/domain/models/paginated_data.dart';
 import 'package:rick_and_morty_app/src/episodes/domain/models/episode.dart';
+import 'package:rick_and_morty_app/src/episodes/domain/models/episodes_filter.dart';
 
 class EpisodeRepository {
   EpisodeRepository({required RickAndMortyApiRepository repository})
       : _repository = repository;
 
-  Future<PaginatedData<Episode>> getAll() {
+  Future<PaginatedData<Episode>> getAll({
+    required int page,
+    required EpisodesFilters filters,
+  }) {
     return _repository.getAll(
       endpoint: ApiEndpoints.episode.path,
       parserFn: Episode.fromJson,
+      queryParams: {...filters.toQueryParams(), 'page': page.toString()},
     );
   }
 
