e90b3acddc2c2d97122ee76f00f5187b8f4d1bb5
Jairo Andrés Portela Cortés <jairo.portela@imagineapps.co>
2023-08-31 23:59:05 -0500

    feat: provide repositories to the app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty_app/src/characters/data/repositories/character_repository.dart';
+import 'package:rick_and_morty_app/src/core/core.dart';
+import 'package:rick_and_morty_app/src/episodes/data/repositories/episode_repository.dart';
 import 'package:rick_and_morty_app/src/home/home_screen.dart';
+import 'package:rick_and_morty_app/src/locations/data/repositories/location_repository.dart';
 
 class RickAndMortyApp extends StatelessWidget {
   const RickAndMortyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final RickAndMortyApiRepository repository = RickAndMortyApiRepository();
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider(
+          create: (context) => CharacterRepository(repository: repository),
+        ),
+        RepositoryProvider(
+          create: (context) => LocationRepository(repository: repository),
+        ),
+        RepositoryProvider(
+          create: (context) => EpisodeRepository(repository: repository),
+        ),
+      ],
+      child: const AppView(),
+    );
+  }
+}
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
 
   @override
   Widget build(BuildContext context) {
