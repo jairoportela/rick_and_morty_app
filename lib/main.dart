9bfb9ecc22892f3893f3139ebaf683bfe28358d5
Jairo Andrés Portela Cortés <jairo.portela@imagineapps.co>
2023-09-01 13:08:55 -0500

    chore: add google fonts documentation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rick_and_morty_app/src/app.dart';
 
 void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+  GoogleFonts.config.allowRuntimeFetching = false;
   runApp(const RickAndMortyApp());
 }
